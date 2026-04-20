4b3eb54790ef6f5e4d8f7e7076a216fa73281aa7
Caylor <caylor777@gmail.com>
2026-04-20 08:39:30 -0400

    Add Navbar component and integrate it into Home page; include navbar styles

---
+++ vite-project/src/components/navbar.tsx
@@ -0,0 +1,20 @@
+import '../css/navbar.css';
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <a>cay1or.dev</a>
+      <div className="navbarLinks">
+        <a href="#about">About</a>
+        <a href="#photography">Photography</a>
+        <a href="#work">Web Design</a>
+        <a href="#services">Software</a>
+      </div>
+      <div className="navbarContact">
+        <a href="#contact">Contact</a>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ vite-project/src/pages/home.tsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/navbar";
+
+function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h1>Welcome to the Main Page</h1>
+      <p>This is the main content of the application.</p>
+    </div>
+  );
+}
+
+export default Home;
