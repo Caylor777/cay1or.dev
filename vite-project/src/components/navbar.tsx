9f416a480bd90305f63f4d78df9a215cf5bc81db
Caylor <caylor777@gmail.com>
2026-04-20 14:20:16 -0400

    chore: update dependencies and improve navbar layout

--- vite-project/src/components/navbar.tsx
+++ vite-project/src/components/navbar.tsx
@@ -2,18 +2,26 @@ import '../css/navbar.css';
 
 function Navbar() {
   return (
+
     <div className="navbar">
-      <a>cay1or.dev</a>
+      
+      <div className="navbarTitle">
+        <a href="#home">cay1or.dev</a>
+      </div>
+
       <div className="navbarLinks">
         <a href="#about">About</a>
         <a href="#photography">Photography</a>
         <a href="#work">Web Design</a>
         <a href="#services">Software</a>
       </div>
+
       <div className="navbarContact">
         <a href="#contact">Contact</a>
       </div>
+
     </div>
+
   );
 }
 
--- vite-project/src/pages/home.tsx
+++ vite-project/src/pages/home.tsx
@@ -4,6 +4,7 @@ function Home() {
   return (
     <div>
       <Navbar />
+      <img className="bg-image" src="../assets/bg.jpg" alt="Background Image"/>
       <h1>Welcome to the Main Page</h1>
       <p>This is the main content of the application.</p>
     </div>
